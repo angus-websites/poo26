7d3f439d2df5d689331dedf78c698aa63d201c2a
Angus <angus.goody@gmail.com>
2026-01-07 21:59:40 +0000

    added Message model

---
+++ app/Models/Message.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Database\Factories\MessageFactory;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Message extends Model
+{
+    /** @use HasFactory<MessageFactory> */
+    use HasFactory;
+    use HasUuids;
+
+    protected $fillable = [
+        'content',
+    ];
+}
