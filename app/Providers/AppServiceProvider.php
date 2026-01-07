ba7e9c839e34030a2907f0def5b83418a9df02b7
Angus <angus.goody@gmail.com>
2026-01-07 00:38:00 +0000

    added Link repo

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Contracts\LinkRepositoryInterface;
+use App\Repositories\LinkRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(
+            LinkRepositoryInterface::class,
+            LinkRepository::class
+        );
     }
 
     /**
---
+++ app/Repositories/LinkRepository.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Contracts\LinkRepositoryInterface;
+use App\Models\Link;
+use Illuminate\Support\Collection;
+
+class LinkRepository implements LinkRepositoryInterface
+{
+    public function create(array $data): Link
+    {
+        return Link::create($data);
+    }
+
+    public function findById(int $id): ?Link
+    {
+        return Link::find($id);
+    }
+
+    public function findBySlug(string $slug): ?Link
+    {
+        return Link::where('slug', $slug)
+            ->where('is_active', true)
+            ->first();
+    }
+
+    public function all(): Collection
+    {
+        return Link::all();
+    }
+
+    public function update(Link $link, array $data): Link
+    {
+        $link->update($data);
+
+        return $link;
+    }
+
+    public function delete(Link $link): bool
+    {
+        return (bool)$link->delete();
+    }
+}
