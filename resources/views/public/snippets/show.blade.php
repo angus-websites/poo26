57dd0a971d7b05c44901143f86803660467f1c7c
Angus <angus.goody@gmail.com>
2026-01-08 16:33:16 +0000

    attempting copy button

--- resources/views/public/snippets/show.blade.php
+++ resources/views/public/snippets/show.blade.php
@@ -5,10 +5,22 @@
             <flux:card class="dark:bg-[#282A36]">
                 <flux:heading class="flex items-center gap-2">Language: {{ $languageName }}
                     <flux:tooltip content="Copy to Clipboard" class="ml-auto">
-                        <flux:button icon="clipboard-document"/>
+                        <flux:button
+                            square
+                            content="{{ __('Copy me to clipboard') }}"
+                            x-on:click="navigator.clipboard?.writeText($el.closest('[???]').value); $el.setAttribute('data-copied', ''); setTimeout(() => $el.removeAttribute('data-copied'), 2000)">
+                            <flux:icon.clipboard variant="outline" class="[[data-copied]_&]:hidden"/>
+                            <flux:icon.clipboard-document-check variant="outline"
+                                                                class="hidden [[data-copied]_&]:block"/>
+                        </flux:button>
                     </flux:tooltip>
+
+
                 </flux:heading>
-                <div class="mt-2">
+
+
+                <flux:separator class="my-5"/>
+                <div id="snippet-content">
                     {!! $htmlContent !!}
                 </div>
             </flux:card>
