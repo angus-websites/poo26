668617ce7e8d1721de1a975f019109cb36892959
Angus <angus.goody@gmail.com>
2026-01-09 15:39:38 +0000

    added XSS test

--- tests/Feature/Messages/CreateMessageTest.php
+++ tests/Feature/Messages/CreateMessageTest.php
@@ -16,3 +16,20 @@ it('doesnt allow empty messages', function () {
     $page->press('Get Link')
         ->assertDontSee('Your message is ready!');
 });
+
+it('doesnt allow cross site scripting', function () {
+
+    $page = visit('/messages/create');
+
+    $page->fill('message', '<script>console.log("Hello world")</script>')
+        ->press('Get Link')
+        ->assertSee('Your message is ready!');
+
+    // Extract the generated link from the page
+    $shortenedUrl = trim($page->text('#short_url'));
+
+    // Assert that visiting the shortened URL does not execute script
+    $newPage = visit($shortenedUrl);
+
+    $newPage->assertNoConsoleLogs();
+});
