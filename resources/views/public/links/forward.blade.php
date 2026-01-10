3261f5217aedd68e78d0c54fd22bd5c3de962d7a
Angus <angus.goody@gmail.com>
2026-01-10 20:31:19 +0000

    Meta Tweak

--- resources/views/public/links/forward.blade.php
+++ resources/views/public/links/forward.blade.php
@@ -1,5 +1,7 @@
-<x-layouts.minimal title="Poo | Simple & Fast URL Shortener for Links, Messages & Code">
+<x-layouts.minimal title="Poo | Your Poo Link is Here!">
     @section('og-image', url('assets/images/core/ogimagelink.jpg'))
+    @section('description', 'You have been sent a Poo link! Click to proceed to the destination.')
+
     <x-page-container>
         <div class="max-w-3xl mx-auto">
             <flux:card class="space-y-6 text-center" x-data x-init="window.location.href = '{{$targetUrl}}'">
